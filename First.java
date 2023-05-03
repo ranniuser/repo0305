7f758d7fe50f27fecd607f5f0edd85836e4cb384
Rajani <121522922+ranniuser@users.noreply.github.com>
2023-05-03 10:22:52 -0400

    Added code in VSCode editor

--- First.java
+++ First.java
@@ -2,7 +2,8 @@
 	public static void main(String[] args)
 	{
 		System.out.println("Hello from Linux");
-		System.out.println("Hello from GitHub");
+		System.out.println("Adding code from GitHub");
+		System.out.println("Adding code in VSCode");
 		
 	}
 
